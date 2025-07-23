336566a847524a95db8ac9460907574f75ac8803
Franiac <d.rebitzer@gmx.de>
2025-07-23 20:40:07 +0200

    Refined Gaming Keymap

--- keyboards/avalanche/v4/keymaps/default/keymap.c
+++ keyboards/avalanche/v4/keymaps/default/keymap.c
@@ -65,17 +65,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [G2] = LAYOUT(
                  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                                KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                 KC_NO,   KC_0,    KC_1,    KC_2,    KC_3,    KC_NO,                                                KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_0,    KC_4,    KC_5,    KC_6,    KC_NO,                                                KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_0,    KC_7,    KC_8,    KC_9,    KC_NO,   KC_TRNS,  KC_TRNS,         KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                 KC_TRNS, KC_0,    KC_1,    KC_2,    KC_3,    KC_LPRN,                                              KC_RPRN, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,
+        KC_CAPS, KC_TRNS, KC_0,    KC_4,    KC_5,    KC_6,    KC_DEL,                                               KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_EQL,  KC_NUM,
+                 KC_TRNS, KC_0,    KC_7,    KC_8,    KC_9,    KC_LCBR, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_RCBR, KC_PGDN, KC_INS,  KC_PGUP, KC_NO,   KC_TRNS,
+                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 [G3] = LAYOUT(
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_NO,                                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_NO,                                                KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,   KC_P0,   KC_P7,   KC_P8,   KC_P9,   KC_NO,   KC_TRNS,  KC_TRNS,         KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_LPRN,                                              KC_RPRN, KC_EXLM, KC_AT,   KC_HASH, UE,      SZ,
+        UC,      KC_TRNS, KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_DEL,                                               KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, AE,      OE,      KC_NO,,
+                 KC_TRNS, KC_P0,   KC_P7,   KC_P8,   KC_P9,   KC_LBRC, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_RBRC, KC_AMPR, KC_ASTR, KC_NO,   EU,      KC_TRNS,
+                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 )
 };
 
